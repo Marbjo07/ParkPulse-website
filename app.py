8473897874938e483713aa4229da38c397f1273f
Marius Bjørhei <marius.bjorhei@gmail.com>
2024-04-25 22:47:16 +0200

    Using a simple flask server to server index.html

---
+++ app.py
@@ -0,0 +1,9 @@
+from flask import Flask
+from flask_cors import CORS
+app = Flask(__name__, static_folder="/")
+CORS(app) # This will enable CORS for all routes
+
+
+@app.route('/')
+def index():
+    return app.send_static_file("index.html")
